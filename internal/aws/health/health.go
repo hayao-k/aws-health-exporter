a5b28a83ed686aa54faef8780d696d68fb14c6d5
hayao-k <30886141+hayao-k@users.noreply.github.com>
2024-05-07 04:59:09 +0000

    feat: Filter events by start, end and lastUpdated time

--- internal/aws/health/health.go
+++ internal/aws/health/health.go
@@ -3,6 +3,8 @@ package health
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/health"
@@ -10,8 +12,12 @@ import (
 )
 
 func setEventFilter(input *health.DescribeEventsForOrganizationInput,
-	service, eventStatus, eventCategory, region, accountId string) {
-	if service != "" || eventStatus != "" || eventCategory != "" || region != "" || accountId != "" {
+	service, eventStatus, eventCategory, region, accountId,
+	startTime, endTime, lastUpdatedTime string,
+) {
+	if service != "" || eventStatus != "" || eventCategory != "" ||
+		region != "" || accountId != "" || startTime != "" ||
+		endTime != "" || lastUpdatedTime != "" {
 		input.Filter = &types.OrganizationEventFilter{}
 		if service != "" {
 			input.Filter.Services = []string{service}
@@ -32,15 +38,58 @@ func setEventFilter(input *health.DescribeEventsForOrganizationInput,
 		if accountId != "" {
 			input.Filter.AwsAccountIds = []string{accountId}
 		}
+		if startTime != "" {
+			input.Filter.StartTime = parseTimeRange(startTime)
+		}
+		if endTime != "" {
+			input.Filter.EndTime = parseTimeRange(endTime)
+		}
+		if lastUpdatedTime != "" {
+			input.Filter.LastUpdatedTime = parseTimeRange(lastUpdatedTime)
+		}
 	}
 }
 
+func parseTimeRange(timeRangeStr string) *types.DateTimeRange {
+	if timeRangeStr == "" {
+		return nil
+	}
+	var from, to string
+	trimmed := strings.Trim(timeRangeStr, "{}")
+	parts := strings.Split(trimmed, ",")
+	for _, part := range parts {
+		if strings.HasPrefix(part, "from=") {
+			from = strings.TrimPrefix(part, "from=")
+		} else if strings.HasPrefix(part, "to=") {
+			to = strings.TrimPrefix(part, "to=")
+		}
+	}
+	timeRange := &types.DateTimeRange{}
+	if from != "" {
+		fromTime, err := time.Parse(time.RFC3339, from)
+		if err == nil {
+			timeRange.From = aws.Time(fromTime)
+		}
+	}
+	if to != "" {
+		toTime, err := time.Parse(time.RFC3339, to)
+		if err == nil {
+			timeRange.To = aws.Time(toTime)
+		}
+	}
+
+	return timeRange
+}
+
 func DescribeEventsForOrganizationInput(
-	service, eventStatus, eventCategory, region, accountId string) *health.DescribeEventsForOrganizationInput {
+	service, eventStatus, eventCategory, region, accountId,
+	startTime, endTime, lastUpdatedTime string,
+) *health.DescribeEventsForOrganizationInput {
 	input := &health.DescribeEventsForOrganizationInput{
 		MaxResults: aws.Int32(100),
 	}
-	setEventFilter(input, service, eventStatus, eventCategory, region, accountId)
+	setEventFilter(input, service, eventStatus, eventCategory, region, accountId,
+		startTime, endTime, lastUpdatedTime)
 	return input
 }
 
